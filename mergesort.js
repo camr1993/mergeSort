af807f65cbe354e1bf1e0f79351b3aa6f4591a53
Cameron Ratliff <cratliff@colgate.edu>
2020-04-07 18:30:54 -0400

    comments

--- mergesort.js
+++ mergesort.js
@@ -1,28 +1,20 @@
-let test = 'hello';
+// split takes an array and splits it into two even arrays
 function split(wholeArray) {
-  console.log('INITIALIZING SPLIT: ', wholeArray);
-
   if (!wholeArray.length || wholeArray.length === 1) {
-    console.log('SPLIT: LENGTH OF 1 OR 0', wholeArray);
     return wholeArray;
   }
 
   // odd cases will return a secondHalf with 1+ length of firstHalf
   const splitLength = Math.floor(wholeArray.length / 2);
-  console.log('SPLIT LENGTH', splitLength);
   const firstHalf = wholeArray.slice(0, splitLength);
   const secondHalf = wholeArray.slice(splitLength);
-  console.log('first halve length: ', firstHalf.length);
-  console.log('SPLIT: COMPLETE', [firstHalf, secondHalf]);
-  //   console.log('SPLIT: COMPLETE', firstHalf, secondHalf);
+
   return [firstHalf, secondHalf];
 }
 
-// arrays need to be sorted in order to use merge
+// merge takes two SORTED arrays and combines them into one sorted array
 function merge(array1, array2) {
   let sorted = [];
-
-  console.log('INITIALIZING MERGING: ', 'ARR1: ', array1, 'ARR2: ', array2);
 
   while (array1.length && array2.length) {
     let num1 = array1[0];
@@ -38,31 +30,29 @@ function merge(array1, array2) {
   } else {
     sorted = [...sorted, ...array2];
   }
-
+  console.log('MERGE HAPPENS: ', sorted); // logging to see recursion
   return sorted;
 }
 
+// mergeSort's goal is to take an array and return the sorted array
+// For every step in recursion, it is going to split the current array in half,
+// and return the merge of each half
+// end result is when the array.length finally gets to 1 for every array,
+// recursion will unwind: merge will get called on [2] and [1] to get [1, 2], then merge will get called on [5] and [1, 2] to get [1, 2, 5]
+// keeps unwinding until the FIRST and SECOND halves are returned inside of the first merge that was called and they too are now merged
+//
+// look at the console logs to see it in action!
 function mergeSort(array) {
+  console.log('mergeSort splits: ', array); // logging to see recursion
   if (array.length < 2) {
     return array;
   }
-
   let halves = split(array);
-
-  //   console.log('THIS IS THE CURRENT ARRAY: ', array);
-  //   console.log('THIS IS THE CURRENT HALVES: ', halves);
 
   return merge(mergeSort(halves[0]), mergeSort(halves[1]));
 }
 
+// test:
 const arr = [6, 21, 7, -2, 1, 100];
 
 console.log(mergeSort(arr));
-
-// split([6, 21, 7]);
-
-const test = [3];
-const test2 = [];
-
-// console.log('TEST1', merge([1, 4, 7], [-1, 6, 7]));
-// console.log('TEST2', split(test2));
